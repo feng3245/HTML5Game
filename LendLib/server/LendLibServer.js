6720764762ae2a696cb868a31ac161cbe4e77bb1
feng3245 <li123b@hotmail.com>
2013-12-06 15:36:01 -0500

    Added multi user support

---
+++ LendLib/server/LendLibServer.js
@@ -0,0 +1,20 @@
+  Meteor.startup(function () {
+  	 
+
+    // code to run on server at startup
+    Meteor.publish("Categories", function() {
+      return lists.find({owner: this.userId },{fields:{Category:1}});
+    });
+    Meteor.publish("listdetails", function(category_id){
+      return lists.find({_id:category_id});
+    });
+    return Meteor.methods({
+
+      removeAllItem: function(stuffitem) {
+
+//        stuffitem.remove({});
+
+      }
+
+    });
+  });
